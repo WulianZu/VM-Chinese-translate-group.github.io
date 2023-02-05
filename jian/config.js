27f3113e02502a4a41e6cd04f0d50321d6b54c88
曲逸 <113536496+yiqv@users.noreply.github.com>
2023-02-05 21:40:23 +0800

    Add files via upload

---
+++ jian/config.js
@@ -0,0 +1,20 @@
+window.Config = {
+  SiteName: 'VM汉化组网站状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+ 'm793481634-3b418b91b37167025c1d1ab7',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'VM汉化组',
+      url: 'https://vmct-cn.top/'
+    },
+  ]
+};
